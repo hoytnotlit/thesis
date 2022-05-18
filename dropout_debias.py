b68cf3c3b66240b4f6175d9896450da50a2010d6
minerva <minerva.suvanto@gmail.com>
2022-05-18 12:29:35 +0200

    Dropout antonym probabilities

--- dropout_debias.py
+++ dropout_debias.py
@@ -7,27 +7,26 @@ model_path = 'test-mlm/'
 def run_bias_analysis():
     # short sentences
     short = data.get_context_sentences(c.context, c.context_t_i, c.context_a_i)
-    #pos_result = data.get_context_sentences(context, context_t_i, context_a_i, pos=True)
+    short_ant = data.get_context_sentences(c.context, c.context_t_i, c.context_a_i, pos=True)
 
     # long sentences
     long = data.get_context_sentences(c.context_long, c.context_long_t_i, c.context_long_a_i)
-    #long_pos_result = data.get_context_sentences(context_long, context_long_t_i, context_long_a_i, pos=True)
+    long_ant = data.get_context_sentences(c.context_long, c.context_long_t_i, c.context_long_a_i, pos=True)
 
     s = sdb.mask_attributes(short)
+    s_a = sdb.mask_attributes(short_ant)
     l = sdb.mask_attributes(long)
+    l_a = sdb.mask_attributes(long_ant)
 
     model, tokenizer = score.get_model(model_path)
     old_model, old_tokenizer = score.get_model() # model to compare to
 
     sdb.save_scores(get_bert_probs(s, model, tokenizer, old_model, old_tokenizer, "s_"), 'dro_short.json')
+    sdb.save_scores(get_bert_probs(s_a, model, tokenizer, old_model, old_tokenizer, "s_", save_dist=False), 'dro_short_ant.json')
     sdb.save_scores(get_bert_probs(l, model, tokenizer, old_model, old_tokenizer, "l_"), 'dro_long.json')
+    sdb.save_scores(get_bert_probs(l_a, model, tokenizer, old_model, old_tokenizer, "l_", save_dist=False), 'dro_long_ant.json')
 
-    #save_scores(score.process_scores(model, tokenizer, result), "short.json")
-    #save_scores(score.process_scores(model, tokenizer, pos_result), "pos_short.json")
-    #save_scores(score.process_scores(model, tokenizer, long_result), "long.json")
-    #save_scores(score.process_scores(model, tokenizer, long_pos_result), "pos_long.json")
-
-def get_bert_probs(sentences, model, tokenizer, orig_model, orig_tokenizer, pref=""):
+def get_bert_probs(sentences, model, tokenizer, orig_model, orig_tokenizer, pref="", save_dist=True):
     result = {}
 
     for eth in sentences:
@@ -40,8 +39,9 @@ def get_bert_probs(sentences, model, tokenizer, orig_model, orig_tokenizer, pref
             orig_inp = score.get_tokenized_sentence(sent, orig_tokenizer)
             orig_probs = score.predict_masked_sent(orig_model, orig_tokenizer, orig_inp)
 
-            # save probability distributions
-            torch.save(new_probs, f"Results/raw/dists/{pref}{eth}_{i}_dro")
+            if save_dist:
+                # save probability distributions
+                torch.save(new_probs, f"Results/raw/dists/{pref}{eth}_{i}_dro")
 
             # track changes of probability on term level
             temp = []
@@ -64,4 +64,3 @@ def save_scores(jscores, file_name):
 
 if __name__ == "__main__":
     run_bias_analysis()
-    # TODO run antonym probabilities
